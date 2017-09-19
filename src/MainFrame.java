22426b4626d16d31ff86ddc7cd75057419a5ecef
battlesushi <105021030@live.asia.edu.tw>
2017-09-19 11:53:28 +0800

    ex

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,2 +1,19 @@
-public class MainFrame {
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class MainFrame extends Frame {
+    public MainFrame(){
+        init();
+    }
+    private void init(){
+        this.setBounds(300,300,500,300);
+        
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
 }
